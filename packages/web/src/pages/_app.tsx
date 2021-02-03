c6ba04b4b149d06f3cdca3e32618bab4165389f4
signorettif <signorettif@users.noreply.github.com>
2021-02-03 21:58:54 +0100

    feat: added inital boilerplate

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import 'styles/antd.css';
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- packages/web/src/pages/logged.tsx
+++ packages/web/src/pages/logged.tsx
@@ -2,30 +2,30 @@ import Head from 'next/head';
 
 // Components
 import { Card, Button } from 'antd';
-import { GoogleOutlined } from '@ant-design/icons';
+import { LogoutOutlined } from '@ant-design/icons';
 
 // Styles
 import styles from 'styles/pages/home.module.scss';
 
-const Home = () => {
+const Logged = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>SaaS boilerplate</title>
+        <title>SaaS boilerplate - Logged in!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1>Sign in to SaaS</h1>
         <Card className={styles.card}>
+          <p>You are logged in</p>
           <Button
             className={styles.googleSignIn}
             type="primary"
             size="large"
-            icon={<GoogleOutlined />}
+            icon={<LogoutOutlined />}
             onClick={() => null}
           >
-            Sign in with Google
+            Log me out!
           </Button>
         </Card>
       </main>
@@ -33,4 +33,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default Logged;
